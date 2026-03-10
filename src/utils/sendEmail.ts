94eac7f04c5c61c00de7c163eb25c145b9bd827a
Mitin Lakhani <mitinlakhani2004@gmail.com>
2026-03-10 11:00:19 +0530

    send grid mail service

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,20 +1,43 @@
-import nodemailer from "nodemailer"
+// import nodemailer from "nodemailer"
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SEND_GRID_API as string);
+
 
 export const sendEmail = async (to:string,subject:string,text:string) =>{
-    const transporter = nodemailer.createTransport({
-       service:"gmail",
-       host:"smtp.gmail.com",
-       port:465,
-       secure:true,
-        auth:{
-            user:process.env.USER_EMAIL,
-            pass:process.env.USER_PASS,
-        },
-    });
-    await transporter.sendMail({
-        from:process.env.USER_EMAIL,
-        to,
-        subject,
-        text
-    })
+
+    try{
+        const msg = {
+            to,
+            from:process.env.USER_EMAIL as string,
+            subject,
+            text,
+        };
+        
+        await sgMail.send(msg);
+        console.log("Mail Sent SuccessFully");
+    }catch(error){
+        console.error("SendGrid Error",error);
+        throw error;
+    }
+
+
+
+
+    // const transporter = nodemailer.createTransport({
+    //    service:"gmail",
+    //    host:"smtp.gmail.com",
+    //    port:465,
+    //    secure:true,
+    //     auth:{
+    //         user:process.env.USER_EMAIL,
+    //         pass:process.env.USER_PASS,
+    //     },
+    // });
+    // await transporter.sendMail({
+    //     from:process.env.USER_EMAIL,
+    //     to,
+    //     subject,
+    //     text
+    // })
 }
